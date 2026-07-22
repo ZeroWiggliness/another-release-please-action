6912a2cd86cb60f474d92c1d414f7d50b4a18393
Darren Vine <darren.vine@goooglemail.com>
2026-07-23 00:48:31 +0200

    fix: Stop defaults from overridding.

--- __fixtures__/core.ts
+++ __fixtures__/core.ts
@@ -5,6 +5,10 @@ export const debug = jest.fn<typeof core.debug>()
 export const error = jest.fn<typeof core.error>()
 export const info = jest.fn<typeof core.info>()
 export const getInput = jest.fn<typeof core.getInput>()
+export const getBooleanInput = jest.fn<typeof core.getBooleanInput>((name) => {
+  const rawValue = getInput(name)
+  return rawValue === 'true' || rawValue === '1'
+})
 export const setOutput = jest.fn<typeof core.setOutput>()
 export const setFailed = jest.fn<typeof core.setFailed>()
 export const warning = jest.fn<typeof core.warning>()
